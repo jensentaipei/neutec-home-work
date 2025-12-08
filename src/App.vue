db8c28252f667c42d00d63cea77e4400d656e5ae
Jensen <jensentaipei@gmail.com>
2025-12-08 21:27:30 +0800

    feat: add grid block

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,28 @@
 import { ref } from 'vue'
 import SideMenu from '@/components/SideMenu.vue'
 import MenuIcon from '@/components/icons/MenuIcon.vue'
+import GridBlock from '@/components/GridBlock.vue'
 
 const menuVisible = ref(false)
+const blinkingBlocks = [3, 5, 9]
 </script>
 
 <template>
   <main class="relative h-screen w-screen bg-gray-50 overflow-hidden">
-    <div class="flex">
-      <div class="flex justify-end w-full pt-5 pr-5">
+    <div class="flex flex-col h-full">
+      <div class="flex justify-end w-full p-5">
         <button @click="menuVisible = !menuVisible">
           <MenuIcon width="20" height="20" />
         </button>
       </div>
+      <div class="flex-1 bg-gray-200 flex items-center justify-center">
+        <div class="grid grid-cols-3 gap-1 w-full p-4">
+          <GridBlock v-for="n in 9" :key="n" :is-blinking="blinkingBlocks.includes(n)" />
+        </div>
+      </div>
     </div>
 
-    <div
-      v-if="menuVisible"
-      class="absolute inset-0 bg-black/20 z-10"
-      @click="menuVisible = false"
-    />
+    <div v-if="menuVisible" class="absolute inset-0 bg-black/0 z-10" @click="menuVisible = false" />
     <Transition name="menu-fade">
       <SideMenu v-show="menuVisible" class="absolute right-0 top-0 z-20" />
     </Transition>
---
+++ src/components/GridBlock.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  isBlinking?: boolean
+}>()
+</script>
+
+<template>
+  <div class="base-block" :class="{ 'animate-blink': isBlinking }" />
+</template>
+
+<style scoped>
+.base-block {
+  height: 100px;
+  border: black solid 2px;
+  background: radial-gradient(circle, rgba(113, 81, 95, 1) 81%, rgba(0, 0, 0, 1) 100%);
+}
+
+.animate-blink {
+  animation: pulse-opacity 0.5s infinite ease-in-out;
+}
+
+@keyframes pulse-opacity {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
